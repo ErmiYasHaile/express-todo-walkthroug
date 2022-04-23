63e60f31964c1205b11322003d2f268a14f64e41
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-04-23 13:09:02 -0700

    update

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ cxn
 ////////////////////////
 // Schemas and Models
 ////////////////////////
+//Schema the definition of our data type
+// model, the object for working with our data type
+const todoSchema = new mongoose.Schema({
+    text: String,
+    completed: Boolean
+},{timestamps:true})
+const Todo =mongoose.model("Todo",todoSchema)
 
 /////////////////////////////
 // Create Express apllication
@@ -46,9 +53,44 @@ app.use("/static", express.static("static")) // serve files statically
 /////////////////////////////
 // Routes
 /////////////////////////////
-app.get("/",(req,res)=>{
-    res.send("<h1>Hello World</h1>")
+app.get("/",async(req,res)=>{
+    // go get todos
+    const todos = await Todo.find({})
+    //render index.ejs
+    // res.send("<h1>Hello World</h1>")
+    res.render("index.ejs", {todos})
 })
+
+app.get("/todo/seed", async (req, res) => {
+    // delete all existing todos
+    await Todo.remove({}).catch((err) => res.send(err))
+    // add you sample todos
+    const todos = await Todo.create([
+        {text: "eat breakfast", completed: false},
+        {text: "eat lunch", completed: false},
+        {text: "eat dinner", completed: false}
+    ]).catch((err) => res.send(err))
+    // send the todos as json
+    res.json(todos)
+})
+
+app.post("/todo", async (req, res) => {
+    // create the todo
+    await Todo.create(req.body).catch((err) => res.send(err))
+    // redirect back to main page
+    res.redirect("/")
+})
+ app.put("/todo/:id", async(req,res)=>{
+     // get the id from the params
+     const id = req.params.id
+     //get the todo to be updated
+     const todo = await Todo.findById(id)
+     //update the todos completed property
+     todo.completed = true
+    todo.save() // save changes
+    // redirect back to main page
+    res.redirect("/")
+ })
 
 ///////////////////////////
 // Server Listener
